905c6dad9a96f68cd85d35cd36b05ff52edc01eb
DEBJIT <112118014@nitt.edu>
2023-12-20 22:47:27 +0530

    updates

--- src/click-me/click-me.component.ts
+++ src/click-me/click-me.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-click-me',
@@ -9,12 +10,22 @@ export class ClickMeComponent {
   clickMessage: string = '';
   result: string = '';
   isThala: boolean = false;
-inputValue: any;
+  inputValue: any;
+  videoSource: string = './../assets/thala.mp4';
+  isVediya: boolean = false;
 
   checkThala() {
     this.isThala = this.clickMessage.length === 7 || this.clickMessage === '7';
-    if(this.isThala) this.result="Thala for a reason";
-    else this.result="Sorry!";
+    if(this.isThala) 
+    {
+      this.result="Thala for a reason😎";
+      this.isVediya=false;
+    }
+    else if(this.clickMessage.length>0 && !this.isThala) 
+    {
+      this.result="Try Again👀!";
+      this.isVediya=true;
+    }
   }
 
 }
